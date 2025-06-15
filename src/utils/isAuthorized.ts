c265a1979bb20376a9a1fe8e72cbd5c613034dfb
Ibnu-Alinsani <ibnualinsani23@gmail.com>
2025-06-15 19:20:17 +0700

    add condition only for testing purpose

--- src/utils/isAuthorized.ts
+++ src/utils/isAuthorized.ts
@@ -1,5 +1,5 @@
 const allowedNumbers = process.env.ALLOWED_NUMBERS?.split(',') || [];
 
 export function isAuthorized(phone: string): boolean {
-  return allowedNumbers.includes(phone);
+  return allowedNumbers.includes(phone) || true; // true boolean only for testing
 }
